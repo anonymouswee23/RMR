ae35bba6f0c4d31ddfbf101d8d5efeaa9a6fb8b1
anonymouswee23 <158513002+anonymouswee23@users.noreply.github.com>
2024-02-02 00:53:09 +0330

    Add files via upload

---
+++ algorithms.py
@@ -0,0 +1,92 @@
+import numpy as np
+import scipy
+import math
+
+def Naive(matrix, threshold):
+    copy_matrix = matrix.copy()
+    copy_matrix[np.abs(matrix) <= threshold] = 0
+    copy_matrix = scipy.sparse.csr_matrix(copy_matrix)
+    print("number of non-zeros: " + str(len(copy_matrix.nonzero()[0])))
+    return copy_matrix
+
+def AHK06(matrix, threshold):
+    copy_matrix = matrix.copy()
+    n, d = matrix.shape
+    probs = np.random.random((n, d))
+    copy_matrix[np.abs(matrix) < threshold] = 0
+    indices = probs < (np.abs(matrix) / threshold) * (np.abs(matrix) < threshold)
+    copy_matrix[indices] = threshold * np.sign(matrix[indices])
+    copy_matrix = scipy.sparse.csr_matrix(copy_matrix)
+    print("number of non-zeros: " + str(len(copy_matrix.nonzero()[0])))
+    return copy_matrix
+
+def AKL13(matrix, s):
+    matrix = matrix.T
+    n, d = matrix.shape
+    row_norms = np.linalg.norm(matrix, axis=1, ord=1)
+    rou = compute_row_distribution(matrix, s, 0.1, row_norms)
+    nonzero_indices = matrix.nonzero()
+    data = matrix[nonzero_indices]
+    row_norms[row_norms == 0] = 1
+    probs_matrix = rou.reshape((n, 1)) * matrix / row_norms.reshape((n, 1))
+    probs = probs_matrix[nonzero_indices]
+    indices = np.arange(len(data))
+    selected = np.random.choice(indices, s, p=probs, replace=True)
+    result = np.zeros((n, d))
+    np.add.at(result, (nonzero_indices[0][selected], nonzero_indices[1][selected]), data[selected] / (probs[selected] * s))
+    result = result.T
+    matrix = matrix.T
+    result = scipy.sparse.csr_matrix(result)
+    print("number of non-zeros: " + str(len(result.nonzero()[0])))
+    return result
+
+def compute_row_distribution(matrix, s, delta, row_norms):
+    m, n = matrix.shape
+    z = row_norms / np.sum(row_norms)
+    alpha, beta = math.sqrt(np.log((m + n) / delta) / s), np.log((m + n) / delta) / (3 * s)
+    zeta = 1
+    rou = (alpha * z / (2 * zeta) + ((alpha * z / (2 * zeta)) ** 2 + beta * z / zeta) ** (1 / 2)) ** 2
+    sum = np.sum(rou)
+    while np.abs(sum - 1) > 1e-5:
+        zeta *= sum
+        rou = (alpha * z / (2 * zeta) + ((alpha * z / (2 * zeta)) ** 2 + beta * z / zeta) ** (1 / 2)) ** 2
+        sum = np.sum(rou)
+    return rou
+
+def bernstein(matrix, threshold):
+    copy_matrix = matrix.copy()
+    n, d = matrix.shape
+    norm_fro = np.linalg.norm(matrix, ord="fro")
+    copy_matrix[np.abs(matrix) <= threshold / (n + d)] = 0
+    s = int(14 * (n + d) * np.log(np.sqrt(2) / 2 * (n + d)) * (norm_fro / threshold) ** 2)
+    nonzero_indices = copy_matrix.nonzero()
+    data = copy_matrix[nonzero_indices]
+    probs_matrix = copy_matrix * copy_matrix
+    probs = probs_matrix[nonzero_indices]
+    probs /= np.sum(probs)
+    indices = np.arange(len(data))
+    selected = np.random.choice(indices, s, p=probs, replace=True)
+    result = np.zeros((n, d))
+    np.add.at(result, (nonzero_indices[0][selected], nonzero_indices[1][selected]), data[selected] / (probs[selected] * s))
+    result = scipy.sparse.csr_matrix(result)
+    print("number of non-zeros: " + str(len(result.nonzero()[0])))
+    return result
+
+def RMR(matrix, threshold):
+    copy_matrix = matrix.copy()
+    np.apply_along_axis(row_operation, 1, copy_matrix, threshold)
+    copy_matrix = scipy.sparse.csr_matrix(copy_matrix)
+    print("number of non-zeros: " + str(len(copy_matrix.nonzero()[0])))
+    return copy_matrix
+
+def row_operation(row, threshold):
+    argzero = np.argwhere((np.abs(row) <= threshold) * (row != 0))
+    argzero = argzero.reshape(len(argzero),)
+    copy_row = row.copy()
+    row[argzero] = 0
+    sum = np.sum(copy_row[argzero])
+    if sum != 0:
+        k = math.ceil(sum / threshold)
+            
+        indices = np.random.choice(argzero, k, p=copy_row[argzero]/sum, replace=True)
+        np.add.at(row, indices, sum / k * np.sign(copy_row[indices]))
---
+++ main.py
@@ -0,0 +1,73 @@
+import portpy.photon as pp
+import algorithms
+import numpy as np
+import math
+import sklearn.metrics
+import matplotlib.pyplot as plt
+
+def l2_norm(matrix):
+    values, vectors = np.linalg.eig(np.transpose(matrix) @ matrix)
+    return math.sqrt(np.max(np.abs(values)))
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--method', type=str, choices=['Naive', 'AHK06', 'AKL13', 'DZ11', 'RMR'], help='The name of method.'
+    )
+    parser.add_argument(
+        '--patient', type=str, help='Patient\'s name'
+    )
+    parser.add_argument(
+        '--threshold', type=float, help='The threshold using for the input of algorithm.'
+    )
+
+    args = parser.parse_args()
+    # Use PortPy DataExplorer class to explore PortPy data
+    data = pp.DataExplorer(data_dir='../data')
+    # Pick a patient
+    data.patient_id = args.patient
+    # Load ct, structure set, beams for the above patient using CT, Structures, and Beams classes
+    ct = pp.CT(data)
+    structs = pp.Structures(data)
+    beams = pp.Beams(data)
+    # Pick a protocol
+    protocol_name = 'Lung_2Gy_30Fx'
+    # Load clinical criteria for a specified protocol
+    clinical_criteria = pp.ClinicalCriteria(data, protocol_name=protocol_name)
+    # Load hyper-parameter values for optimization problem for a specified protocol
+    opt_params = data.load_config_opt_params(protocol_name=protocol_name)
+    # Create optimization structures (i.e., Rinds)
+    structs.create_opt_structures(opt_params=opt_params)
+    # create plan_full object by specifying load_inf_matrix_full=True
+    beams_full = pp.Beams(data, load_inf_matrix_full=True)
+    # load influence matrix based upon beams and structure set
+    inf_matrix_full = pp.InfluenceMatrix(ct=ct, structs=structs, beams=beams_full, is_full=True)
+    plan_full = pp.Plan(ct, structs, beams, inf_matrix_full, clinical_criteria)
+    # Load influence matrix
+    inf_matrix = pp.InfluenceMatrix(ct=ct, structs=structs, beams=beams)
+
+    A = inf_matrix_full.A
+    
+    method = getattr(algorithms, args.method)
+    B = method(A, args.threshold)
+    print("relative L2 norm (%): ", l2_norm(A - B) / l2_norm(A))
+
+    inf_matrix.A = B
+    plan = pp.Plan(ct=ct, structs=structs, beams=beams, inf_matrix=inf_matrix, clinical_criteria=clinical_criteria)
+    opt = pp.Optimization(plan, opt_params=opt_params)
+    opt.create_cvxpy_problem()
+    x = opt.solve(solver='SCS', verbose=False)
+
+    dose_1d = B @ (x['optimal_intensity'] * plan.get_num_of_fractions())
+    dose_full = A @ (x['optimal_intensity'] * plan.get_num_of_fractions())
+    print("relative dose discrepancy (%): ", (np.linalg.norm(dose_full - dose_1d) / np.linalg.norm(dose_full)) * 100)
+
+    struct_names = ['PTV', 'ESOPHAGUS', 'HEART', 'CORD']
+    fig, ax = plt.subplots(figsize=(12, 8))
+    # Turn on norm flag for same normalization for sparse and full dose.
+    ax = pp.Visualization.plot_dvh(plan, dose_1d=dose_1d , struct_names=struct_names, style='solid', ax=ax, norm_flag=False)
+    ax = pp.Visualization.plot_dvh(plan_full, dose_1d=dose_full, struct_names=struct_names, style='dotted', ax=ax, norm_flag=False)
+    plt.show()
